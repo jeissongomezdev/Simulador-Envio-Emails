59422a3be35e5034ee1197c531ef8d19c2f8995e
Jeisson Gómez <jeissongomez@hotmail.es>
2022-10-19 22:31:03 -0500

    Se agrega alerta de error de validacion y se agrega al HTML

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#enviar-mail");
 
   // Asignar eventos
   inputEmail.addEventListener("blur", validar);
@@ -10,6 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
   inputMensaje.addEventListener("blur", validar);
 
   function validar(e) {
-    console.log(e.target.value);
+    if (e.target.value.trim() === "") {
+      mostrarAlerta();
+    } else {
+      console.log("Hay algo");
+    }
+  }
+
+  function mostrarAlerta() {
+    // Generar alerta en HTML
+    const error = document.createElement("P");
+    error.textContent = "Ocurrió un error";
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+    // Inyectar el error al formulario
+    formulario.appendChild(error);
   }
 });
