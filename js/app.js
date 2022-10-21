6c4c01c7b157287d81f41447bc7da8da097b0fc5
Jeisson Gómez <jeissongomez@hotmail.es>
2022-10-20 20:56:57 -0500

    Mostrando alertas junto a sus campos

--- js/app.js
+++ js/app.js
@@ -12,19 +12,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function validar(e) {
     if (e.target.value.trim() === "") {
-      mostrarAlerta(`El campo ${e.target.id} es obligatorio`);
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
     } else {
       console.log("Hay algo");
     }
   }
 
-  function mostrarAlerta(mensaje) {
+  function mostrarAlerta(mensaje, referencia) {
     // Generar alerta en HTML
     const error = document.createElement("P");
     error.textContent = mensaje;
     error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
 
     // Inyectar el error al formulario
-    formulario.appendChild(error);
+    referencia.appendChild(error);
   }
 });
