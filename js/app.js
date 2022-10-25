4d60d3624fa704dbd55b3556cb4203cd1d05d33d
Jeisson Gómez <jeissongomez@hotmail.es>
2022-10-24 19:11:00 -0500

    Se añade spinner

--- js/app.js
+++ js/app.js
@@ -12,11 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.querySelector("#formulario");
   const btnSubmit = document.querySelector('#formulario button[type="submit"]');
   const btnReset = document.querySelector('#formulario button[type="reset"]');
+  const spinner = document.querySelector("#spinner");
 
   // Asignar eventos
   inputEmail.addEventListener("input", validar);
   inputAsunto.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
+
+  formulario.addEventListener("submit", enviarEmail);
 
   btnReset.addEventListener("click", function (e) {
     e.preventDefault();
@@ -29,6 +32,13 @@ document.addEventListener("DOMContentLoaded", function () {
     formulario.reset();
     comprobarEmail();
   });
+
+  function enviarEmail(e) {
+    e.preventDefault();
+
+    spinner.classList.add("flex");
+    spinner.classList.remove("hidden");
+  }
 
   function validar(e) {
     if (e.target.value.trim() === "") {
